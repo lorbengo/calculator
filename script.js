da701a1b79175cef64eefc0d19061d953a0205e1
lorbengo <lorbengo@gmail.com>
2024-02-21 17:12:43 +0300

    Add four basic math operators functions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hi");
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+  return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+  return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+  return Number(a) / Number(b);
+}
